34fb49a26d47bfecdae2f020cfcb1d1334e34956
ahmed hussien <33827384+eng-ahmedhussien@users.noreply.github.com>
2023-08-13 16:43:31 +0300

    Add files via upload

---
+++ DeletePopView.swift
@@ -0,0 +1,46 @@
+//
+//  PopView.swift
+//  AlDwaaNewApp
+//
+//  Created by ahmed hussien on 10/08/2023.
+//
+
+import SwiftUI
+
+struct DeletePopView<Content:View>:View {
+    
+    var closePopup: () -> Void
+    @ViewBuilder var  content:Content
+    var titel = ""
+    var mesaage = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                //close Btn
+                Button(action: {
+                    closePopup()
+                }) {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.gray)
+                }
+            }
+            
+            Text(titel)
+                .font(.headline)
+                .padding()
+            
+            Text(mesaage)
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            content
+            
+        }
+        .padding()
+        .background(Color.theme.bgWhite)
+        .cornerRadius(20)
+    }
+}
